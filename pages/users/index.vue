cb5433abccc882481a522cf94f09a346d0480fbb
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2020-10-31 19:39:44 +0330

    profile list

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <json-viewer dir=ltr :value="profileList"/>
+  <v-row>
+    <v-col>
+      <v-simple-table>
+        <thead>
+        <tr>
+          <th>نام</th>
+          <th>نام خانوادگی</th>
+          <th>ایمیل</th>
+          <th>موبایل</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item,key) in profileList" :key="key" @click="goto(item.user_id)">
+          <td>{{ item.name }}</td>
+          <td>{{ item.last_name }}</td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.cell_phone }}</td>
+        </tr>
+        </tbody>
+      </v-simple-table>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -11,6 +32,11 @@ export default {
   },
   async fetch() {
     this.profileList = await this.$axios.$get('/profiles');
+  },
+  methods: {
+    goto($id) {
+      this.$router.push('/Users/' + $id)
+    }
   }
 }
 </script>
