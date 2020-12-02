ece8c6af60a85282d9c6bdb72df057db382fe312
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2020-12-02 23:04:01 +0330

    new class just in case

---
+++ components/PromiseBtn.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-btn :loading="loadingState" @click="onPromise"></v-btn>
+</template>
+
+<script>
+import ps from '@/mixins/progress.stopper'
+
+export default {
+  mixins: [ps],
+  name: "PromiseBtn",
+  props: {
+    promise: Promise,
+    func: Function
+  },
+  data() {
+    return {
+      loadingState: false
+    }
+  },
+  methods: {
+    onPromise() {
+      this.loadingState = true
+      this.promise
+          .then(res => this.func(res))
+          .finally(() => this.loadingState = false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ mixins/progress.stopper.js
@@ -0,0 +1,8 @@
+export default {
+    errorCaptured(err, vm, info) {
+        console.log('errrror captured')
+        for (let item of Object.keys(this.l)) {
+            this.l[item] = false
+        }
+    }
+}
