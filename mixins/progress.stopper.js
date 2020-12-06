947e69136902ea8e10c06ee4e1d8e4e89880bb2c
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2020-12-06 13:23:45 +0330

    fix stepper

--- mixins/progress.stopper.js
+++ mixins/progress.stopper.js
@@ -2,7 +2,7 @@ export default {
     errorCaptured(err, vm, info) {
         console.log('errrror captured')
         for (let item of Object.keys(this.l)) {
-            this.l[item] = false
+            this.l.item = false
         }
     }
 }
