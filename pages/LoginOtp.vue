a865e9c46de671ec250305c3e939c09c9bb78677
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2020-12-26 00:21:35 +0330

    add progress to otp

--- pages/LoginOtp.vue
+++ pages/LoginOtp.vue
@@ -1,7 +1,7 @@
 <template>
   <login-reg-card title="تایید کد دو عاملی"
                   subtitle="برای ایمن سازی حساب کاربری کد ۶رقمی که به موبایل شما ارسال شده را در کادر زیر وارد نمایید.">
-    <otp class="mt-8" @send="onSubmit" @otp="token=$event" label="کد تایید"/>
+    <otp :loading="l.submit" class="mt-8" @send="onSubmit" @otp="token=$event" label="کد تایید"/>
   </login-reg-card>
 </template>
 
