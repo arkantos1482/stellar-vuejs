47c75e817c9dce0671afc9b43449f2965fa1cc04
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2021-01-08 03:08:12 +0330

    minus selling

--- store/balances.js
+++ store/balances.js
@@ -12,7 +12,7 @@ export const actions = {
     async refresh(context) {
         let balances = (await this.$axios.$get('/profiles/me/stellar')).balances
         let result = collect(balances)
-            .map(item => ({[item.asset_code]: item.balance}))
+            .map(item => ({[item.asset_code]: item.balance - item.selling_liabilities}))
             .reduce((_acc, item) => ({..._acc, ...item})) ?? []
 
         context.commit('set', result)
