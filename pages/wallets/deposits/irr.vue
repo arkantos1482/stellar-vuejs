80e4762903af57ca1367640d84c57a1e4f644644
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2021-01-13 12:26:03 +0330

    deposit remained

--- pages/wallets/deposits/irr.vue
+++ pages/wallets/deposits/irr.vue
@@ -2,7 +2,14 @@
   <div class="d-flex align-items-stretch mt-8">
     <a-card class="ml-4 py-8" width="45%" title="واریز">
       <crypto-upper :balance="balance" :type="type"/>
-      <v-form @submit.prevent="onDeposit" v-model="v.deposit" ref="form">
+      <p class="text-display-2 ma-0">
+        باقی مانده واریز روزانه:<span class="font-weight-medium">&nbsp{{ daily_rem_usage }}</span>
+      </p>
+      <p class="text-display-2 ma-0">باقی مانده واریز ماهیانه:
+        <span class="font-weight-medium">&nbsp{{ monthly_rem_usage }}</span>
+      </p>
+
+      <v-form class="mt-12" @submit.prevent="onDeposit" v-model="v.deposit" ref="form">
         <a-text-field mask="####################"
                       :rules="[rules.required,rules.moreThanBillion]"
                       hint="حداقل میزان واریز ۱میلیون ریال می باشد." v-model="amount" filled label="مقدار ریال"/>
@@ -22,6 +29,7 @@ import Deposits from "@/pages/wallets/deposits/index";
 import ACard from "@/components/ACard";
 import ATextField from "@/components/ATextField";
 import CryptoUpper from "@/components/CryptoUpper";
+import {toSeparated} from "@/models/NumberUtil";
 
 export default {
   mixins: [ps],
@@ -47,6 +55,11 @@ export default {
   mounted() {
     this.$store.dispatch('balances/refresh')
     this.$store.dispatch('addresses/refresh')
+    this.$axios.$post('/access/limits/remained', {resource: 'irr', action: 'deposit'})
+        .then(res => {
+          this.daily_rem_usage = (res.daily_rem_usage !== -1) ? toSeparated(res.daily_rem_usage) + 'ریال' : 'نامحدود'
+          this.monthly_rem_usage = (res.monthly_rem_usage !== -1) ? toSeparated(res.monthly_rem_usage) + 'ریال' : 'نامحدود'
+        })
   },
   methods: {
     async onDeposit() {
