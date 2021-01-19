20b101e04dd19cb2c38c3dc5d1728193e645731d
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2021-01-19 20:00:32 +0330

    add pagination to users list

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -7,6 +7,11 @@
           :items="profileList"
           items-per-page="50"
           hide-default-footer/>
+      <v-pagination class="mt-4"
+          v-model="pagination.current"
+          :length="pagination.total"
+          @input="onPageChange"
+      ></v-pagination>
     </v-col>
   </v-row>
 </template>
@@ -22,17 +27,29 @@ export default {
         {text: 'نام خانوادگی', value: 'last_name'},
         {text: 'سطح دسترسی', value: 'access_level'},
         {text: 'موبایل', value: 'cell_phone'}],
-      profileList: []
+      profileList: [],
+      pagination: {
+        current: 1,
+        total: 0
+      }
     }
   },
   async mounted() {
-    let list = await this.$axios.$get('/profiles')
-    this.profileList = list.map((item, index) => ({...item, index: index + 1}))
+    await this.getUsers()
   },
   methods: {
+    async onPageChange() {
+      await this.getUsers()
+    },
+    async getUsers() {
+      let listWithMeta = await this.$axios.$get('/profiles?page=' + this.pagination.current)
+      this.pagination.current = listWithMeta.current_page
+      this.pagination.total = listWithMeta.last_page
+      this.profileList = listWithMeta.data.map((item, index) => ({...item, index: index + 1}))
+    },
     goto($item) {
       this.$router.push('/Users/' + $item.user_id)
-    }
+    },
   }
 }
 </script>
