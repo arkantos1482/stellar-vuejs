11c680739b2fb08428e932eb3c2ce5a740ec3ca0
Abbas Mostakhdemin Hosseini <arkantos1482@gmail.com>
2021-01-29 17:02:38 +0330

    fix tv

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,7 @@
 export default function ({store, route, redirect}) {
-    console.log('auth middleware')
     const myRoute = route.fullPath.toLowerCase();
     if (!store.state.auth.isLoggedIn) {
         if ('/login' !== myRoute && '/register' !== myRoute && '/otp' !== myRoute) {
-            console.log('IF auth middleware !!')
             // return redirect('/login')
         }
     }
