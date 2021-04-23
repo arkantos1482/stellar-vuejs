f07147037e8c5709b09956d4f08a99882eab0c9c
abbas <arkantos1482@gmail.com>
2021-04-24 02:45:07 +0430

    wallet

--- pages/wallets/index.vue
+++ pages/wallets/index.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-card width="100%" class="text-center">
-    <v-simple-table>
-      <thead>
-      <tr>
-        <th class="text-center">ارز</th>
-        <th class="text-center">موجودی</th>
-        <th class="text-center">موجودی در دسترس</th>
-        <th class="text-center">سفارش در جریان</th>
-        <th class="text-center">ارزش BTC</th>
-        <th class="text-center">عملیات</th>
-      </tr>
-      </thead>
-      <tbody>
-      <crypto-address-tr namad="امین" type="amn" :balance="balances.AMN"/>
-<!--      <crypto-address-tr namad="برگ" type="ebg" :balance="balances.EBG"/>-->
-      <crypto-address-tr namad="آرت کوین" type="art" :balance="balances.ART"/>
-      <crypto-address-tr namad="زریک" type="zrk" :balance="balances.ZRK"/>
-<!--      <crypto-address-tr namad="شکوفه آلو" type="sha" :balance="balances.SHA"/>-->
-      <crypto-address-tr namad="تومان" type="irr" :balance="balances.IRR"/>
+  <div class="pa-4">
+    <p class="text-h4 mb-6 text-right">کیف پول شما</p>
+    <v-card width="100%" height="100%" class="px-8 py-4">
+      <v-simple-table>
+        <thead>
+        <tr>
+          <th class="text-center">ارز</th>
+          <th class="text-center">موجودی</th>
+          <th class="text-center">موجودی در دسترس</th>
+          <th class="text-center">سفارش در جریان</th>
+          <th class="text-center">ارزش BTC</th>
+          <th class="text-center">عملیات</th>
+        </tr>
+        </thead>
+        <tbody>
+        <crypto-address-tr namad="امین" type="amn" :balance="balances.AMN"/>
+        <!--      <crypto-address-tr namad="برگ" type="ebg" :balance="balances.EBG"/>-->
+        <crypto-address-tr namad="آرت کوین" type="art" :balance="balances.ART"/>
+        <crypto-address-tr namad="زریک" type="zrk" :balance="balances.ZRK"/>
+        <!--      <crypto-address-tr namad="شکوفه آلو" type="sha" :balance="balances.SHA"/>-->
+        <crypto-address-tr namad="تومان" type="irr" :balance="balances.IRR"/>
 
-<!--      <crypto-address-tr namad="بیت کوین" type="btc" :balance="balances.BTC"/>-->
-<!--      <crypto-address-tr namad="اتریوم" type="eth" :balance="balances.ETH"/>-->
-<!--      <crypto-address-tr namad="لایت کوین" type="ltc" :balance="balances.LTC"/>-->
-<!--      <crypto-address-tr namad="تتر" type="usdt" :balance="balances.USDT"/>-->
-<!--      <crypto-address-tr namad="بیت کوین کش" type="bch" :balance="balances.BCH"/>-->
-      </tbody>
-    </v-simple-table>
-  </v-card>
+        <!--      <crypto-address-tr namad="بیت کوین" type="btc" :balance="balances.BTC"/>-->
+        <!--      <crypto-address-tr namad="اتریوم" type="eth" :balance="balances.ETH"/>-->
+        <!--      <crypto-address-tr namad="لایت کوین" type="ltc" :balance="balances.LTC"/>-->
+        <!--      <crypto-address-tr namad="تتر" type="usdt" :balance="balances.USDT"/>-->
+        <!--      <crypto-address-tr namad="بیت کوین کش" type="bch" :balance="balances.BCH"/>-->
+        </tbody>
+      </v-simple-table>
+    </v-card>
+  </div>
 </template>
 
 <script>
