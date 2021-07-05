1f766c681902f750b16c4675535cd440376ff9b4
abbas <arkantos1482@gmail.com>
2021-07-05 11:44:03 +0430

    bardasht

--- pages/index.vue
+++ pages/index.vue
@@ -18,12 +18,16 @@
         <v-card width="100%" height="100%" class="pa-6">
           <card-title-with-chevron icon="mdi-account" title="وضعیت حساب کاربری"/>
           <v-divider class="my-6"/>
-          <RowItem title="برداشت روزانه ریال" value="۰از۰تومان"/>
-          <RowItem title="برداشت روزانه رمزارز" value="۰از۰تومان"/>
-          <RowItem title="مجموع برداشت روزانه" value="۰از۰تومان"/>
-          <RowItem title="مجموع برداشت ماهانه" value="۰از۰تومان"/>
-          <RowItem title="کارمزد" value="۰از۰تومان"/>
-          <RowItem title="ارزش معاملات سی روز" value="۰از۰تومان"/>
+          <RowItem title="برداشت روزانه ریال"
+                   :value="azMaker(rialLimits.daily_usage,rialLimits.daily_max_usage)"/>
+          <RowItem title="برداشت روزانه رمزارز"
+                   :value="azMaker(cryptoLimits.daily_usage,cryptoLimits.daily_max_usage)"/>
+          <RowItem title="برداشت ماهانه ریال"
+                   :value="azMaker(rialLimits.monthly_usage,rialLimits.monthly_max_usage)"/>
+          <RowItem title="برداشت ماهانه رمزارز"
+                   :value="azMaker(cryptoLimits.monthly_usage,cryptoLimits.monthly_max_usage)"/>
+          <RowItem title="کارمزد" :value="cryptoLimits.fee_ratio"/>
+          <RowItem title="ارزش معاملات سی روز" :value="cryptoLimits.trade_volume_past_month"/>
           <v-divider class="my-6"/>
           <p class="primary--text">سطح کاربری:</p>
           <a-row class="justify-center align-center">
@@ -52,9 +56,9 @@
           <dash-wallet-row :balance="balances.USDT"/>
           <dash-wallet-row :balance="balances.BCH"/>
           <v-divider class="my-6"/>
-<!--          <p class="primary&#45;&#45;text">ارزش تخمینی دارایی ها:</p>-->
-<!--          <RowItem title="پیشنهادهای خرید" value="۱۲۳.۱۲۳.۱۲۳ تومان"/>-->
-<!--          <RowItem title="پیشنهادهای فروش" value="۱۲۳.۱۲۳.۱۲۳ تومان"/>-->
+          <!--          <p class="primary&#45;&#45;text">ارزش تخمینی دارایی ها:</p>-->
+          <!--          <RowItem title="پیشنهادهای خرید" value="۱۲۳.۱۲۳.۱۲۳ تومان"/>-->
+          <!--          <RowItem title="پیشنهادهای فروش" value="۱۲۳.۱۲۳.۱۲۳ تومان"/>-->
         </v-card>
       </v-col>
     </a-row>
@@ -123,15 +127,28 @@ export default {
   data() {
     return {
       accessLevel: '',
-      stats: ''
+      stats: '',
+      rialLimits: {},
+      cryptoLimits: {}
     }
   },
-  async mounted() {
-    let user = await this.$axios.$get('/profiles/me')
-    this.accessLevel = user.access_level
+  mounted() {
+    this.$axios.$get('/profiles/me')
+        .then(res => this.accessLevel = res.access_level)
 
-    let res = await this.$axios.$get('https://api.nobitex.ir/market/stats?srcCurrency=btc,eth,ltc,usdt,bch,bnb,eos,xlm,etc,trx,pmn,doge&dstCurrency=rls,usdt')
-    this.stats = res.stats
+    this.$axios.$post('/access/limits/remained', {resource: 'crypto'})
+        .then(res => this.cryptoLimits = res)
+    this.$axios.$post('/access/limits/remained', {resource: 'irr'})
+        .then(res => this.rialLimits = res)
+
+    this.$axios.$get('https://api.nobitex.ir/market/stats?srcCurrency=btc,eth,ltc,usdt,bch,bnb,eos,xlm,etc,trx,pmn,doge&dstCurrency=rls,usdt')
+        .then(res => this.stats = res.stats)
+  },
+  methods: {
+    azMaker(first, second) {
+      if (first === undefined || second === undefined) return ''
+      return first + ' از ' + second
+    }
   }
 }
 </script>
