343da0674351f15a1a44d90d6e3dac6c68eba55d
abbas <arkantos1482@gmail.com>
2021-08-01 23:56:29 +0430

    update sync scripts

--- sync.sh
+++ sync.sh
@@ -1,1 +1,2 @@
-rsync -av --delete --exclude=node_modules --exclude=.nuxt ./ abbas@192.168.1.6:/home/abbas/workspace/laravel/nuxt-vuetify/
+rsync -av --delete --exclude=node_modules --exclude=.nuxt \
+  ./ abbas@192.168.1.6:/home/abbas/workspace/bitra/vuejs/
