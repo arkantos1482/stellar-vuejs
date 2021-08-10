2188d137b862069e8f9e7b1874f0e9238bd2e1af
abbas <arkantos1482@gmail.com>
2021-08-11 02:30:23 +0430

    withdraw bug fix

--- pages/admin/access/min-trades.vue
+++ pages/admin/access/min-trades.vue
@@ -1,8 +1,11 @@
 <template>
   <v-row justify="center">
     <v-col cols="6">
-      <v-data-table :headers="headers" :items="items" @click:row="showDialog"/>
-      <v-btn color="primary" :click="d.put = true">تغییر/اضافه</v-btn>
+      <v-data-table :headers="headers" :items="items" @click:row="showDialog"
+                    hide-default-footer items-per-page="200"/>
+      <div class="text-center mt-4">
+        <v-btn color="primary" @click="d.put = true" class="px-8">تغییر/اضافه</v-btn>
+      </div>
     </v-col>
 
     <v-dialog width="400" v-model="d.put">
@@ -10,7 +13,7 @@
         <v-select outlined label="رمزارز پایه" :items="coinList" v-model="req.base_coin"/>
         <v-select outlined label="رمزارز مقصد" :items="coinList" v-model="req.counter_coin"/>
         <a-text-field label="مینیمم" v-model="req.min"/>
-        <v-btn :loading="l.put" @click="store">ارسال</v-btn>
+        <v-btn :loading="l.put" @click="store" color="primary" class="mt-4">ثبت</v-btn>
       </v-card>
     </v-dialog>
   </v-row>
@@ -48,6 +51,7 @@ export default {
       this.l.put = true
       await this.$axios.$post('/access/min-trade', this.req)
       await this.refresh()
+      this.d.put = false
       this.l.put = false
     },
     showDialog(item) {
--- pages/admin/access/min-withdraws.vue
+++ pages/admin/access/min-withdraws.vue
@@ -1,15 +1,18 @@
 <template>
   <v-row justify="center">
     <v-col cols="6">
-      <v-data-table :headers="headers" :items="items" @click:row="showDialog"/>
-      <v-btn color="primary" :click="d.put = true">تغییر/اضافه</v-btn>
+      <v-data-table :headers="headers" :items="items" @click:row="showDialog"
+                    hide-default-footer items-per-page="200"/>
+      <div class="text-center mt-4">
+        <v-btn color="primary" @click="d.put = true" class="px-8">تغییر/اضافه</v-btn>
+      </div>
     </v-col>
 
     <v-dialog width="400" v-model="d.put">
       <v-card class="pa-6 text-center">
         <v-select outlined label="رمزارز" :items="coinList" v-model="req.coin"/>
         <a-text-field label="مینیمم" v-model="req.min"/>
-        <v-btn :loading="l.put" @click="store">ارسال</v-btn>
+        <v-btn :loading="l.put" @click="store" color="primary" class="mt-4">ثبت</v-btn>
       </v-card>
     </v-dialog>
   </v-row>
@@ -46,6 +49,7 @@ export default {
       this.l.put = true
       await this.$axios.$post('/access/min-withdraw', this.req)
       await this.refresh()
+      this.d.put = false
       this.l.put = false
     },
     showDialog(item) {
