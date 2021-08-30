36bbc6139dbc2b1c5ae71f9be17718edd7649e8f
abbas <arkantos1482@gmail.com>
2021-08-30 11:55:00 +0430

    add doge

--- pages/wallets/index.vue
+++ pages/wallets/index.vue
@@ -31,7 +31,7 @@
         <crypto-address-tr namad="تتر" type="usdt" :balance="balances.USDT"/>
         <crypto-address-tr namad="ترون" type="trx" :balance="balances.TRX"/>
         <crypto-address-tr namad="بیت کوین کش" type="bch" :balance="balances.BCH"/>
-<!--        <crypto-address-tr namad="دوج کوین" type="doge" :balance="balances.DOGE"/>-->
+        <crypto-address-tr namad="دوج کوین" type="doge" :balance="balances.DOGE"/>
         </tbody>
       </v-simple-table>
     </v-card>
