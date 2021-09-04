cfef30c320350059b29dab3d999d17257eecd20c
abbas <arkantos1482@gmail.com>
2021-09-04 21:21:16 +0430

    trade fee from 0.002 to 0.005

--- components/MyTradesTable.vue
+++ components/MyTradesTable.vue
@@ -79,9 +79,9 @@ export default {
       //   return 0
       // }
       return item.op_type === 'buy'
-          ? toSeparated(this.adjustDp(safeDecimal(item.counter_amount).times(0.002),
+          ? toSeparated(this.adjustDp(safeDecimal(item.counter_amount).times(0.005),
               item.base_asset_code, item.counter_asset_code))
-          : toSeparated(this.adjustDp(safeDecimal(item.base_amount).times(0.002),
+          : toSeparated(this.adjustDp(safeDecimal(item.base_amount).times(0.005),
               item.counter_asset_code, item.base_asset_code))
     },
     adjustDp(val, base, ctr) {
