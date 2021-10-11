312a7b8fe16af63f703cfb1145d6e372a0a5c8a9
abbas <arkantos1482@gmail.com>
2021-10-11 15:20:05 +0330

    add send message to profile

--- pages/admin/verify/_id.vue
+++ pages/admin/verify/_id.vue
@@ -1,56 +1,71 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="9">
-      <v-carousel>
-        <v-carousel-item contain :src="docs.ssn"/>
-        <v-carousel-item contain :src="docs.bill"/>
-        <v-carousel-item contain :src="docs.bank_card"/>
-      </v-carousel>
+  <div>
+    <v-row justify="center">
+      <v-col cols="9">
+        <v-carousel>
+          <v-carousel-item contain :src="docs.ssn"/>
+          <v-carousel-item contain :src="docs.bill"/>
+          <v-carousel-item contain :src="docs.bank_card"/>
+        </v-carousel>
 
-      <div class="text-center mt-8">
-        <v-btn v-if="hasVideo" @click="downloadVideo"
-               :loading="l.downloadVideo" color="primary">دانلود ویدیو
-        </v-btn>
-      </div>
+        <div class="text-center mt-8">
+          <v-btn v-if="hasVideo" @click="downloadVideo"
+                 :loading="l.downloadVideo" color="primary">دانلود ویدیو
+          </v-btn>
+        </div>
 
-      <h1 class="mt-8 mb-2">مشخصات استعلامی</h1>
-      <v-row>
-        <v-col v-for="(item,idx) in kycList" :key="idx" cols="6">
-          <kyc-item :user-id="userId" :title="item.name" :link-suffix="item.ls"/>
-        </v-col>
-      </v-row>
+        <div class="text-center mt-8">
+          <v-btn @click="onMessage">ارسال پیام</v-btn>
+        </div>
 
-      <h1 class="mt-16">تایید مدارک</h1>
-      <v-row>
-        <v-col cols="4">
-          <h3>اطلاعات شخصی</h3>
-          <v-switch label="ایمیل" v-model="state.email"/>
-          <v-switch label="موبایل" v-model="state.cell_phone"/>
-          <v-switch label="کارت ملی" v-model="state.ssn"/>
-          <v-switch label="تصویر هویت" v-model="state.bank_account"/>
-        </v-col>
+        <h1 class="mt-8 mb-2">مشخصات استعلامی</h1>
+        <v-row>
+          <v-col v-for="(item,idx) in kycList" :key="idx" cols="6">
+            <kyc-item :user-id="userId" :title="item.name" :link-suffix="item.ls"/>
+          </v-col>
+        </v-row>
 
-        <v-col cols="4">
-          <h3>اطلاعات تکمیلی</h3>
-          <v-switch label="تلفن ثابت" v-model="state.phone"/>
-          <v-switch label="آدرس" v-model="state.address"/>
-        </v-col>
+        <h1 class="mt-16">تایید مدارک</h1>
+        <v-row>
+          <v-col cols="4">
+            <h3>اطلاعات شخصی</h3>
+            <v-switch label="ایمیل" v-model="state.email"/>
+            <v-switch label="موبایل" v-model="state.cell_phone"/>
+            <v-switch label="کارت ملی" v-model="state.ssn"/>
+            <v-switch label="تصویر هویت" v-model="state.bank_account"/>
+          </v-col>
 
-        <v-col cols="4">
-          <h3>اطلاعات مالی</h3>
-          <v-switch label="شماره کارت" v-model="state.bank_card"/>
-          <v-switch label="شماره کارت ۲" v-model="state.bank_card_2"/>
-          <v-switch label="شبا" v-model="state.bank_shaba"/>
-          <v-switch label="شبا ۲" v-model="state.bank_shaba_2"/>
-        </v-col>
-      </v-row>
+          <v-col cols="4">
+            <h3>اطلاعات تکمیلی</h3>
+            <v-switch label="تلفن ثابت" v-model="state.phone"/>
+            <v-switch label="آدرس" v-model="state.address"/>
+          </v-col>
 
-      <div class="text-center">
-        <v-btn @click="apply" :loading="l.apply" color="red">اعمال تغییر</v-btn>
-        <v-btn @click="$router.back()">بازگشت</v-btn>
-      </div>
-    </v-col>
-  </v-row>
+          <v-col cols="4">
+            <h3>اطلاعات مالی</h3>
+            <v-switch label="شماره کارت" v-model="state.bank_card"/>
+            <v-switch label="شماره کارت ۲" v-model="state.bank_card_2"/>
+            <v-switch label="شبا" v-model="state.bank_shaba"/>
+            <v-switch label="شبا ۲" v-model="state.bank_shaba_2"/>
+          </v-col>
+        </v-row>
+
+        <div class="text-center">
+          <v-btn @click="apply" :loading="l.apply" color="red">اعمال تغییر</v-btn>
+          <v-btn @click="$router.back()">بازگشت</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-dialog width="400" v-model="d.sendMessage">
+      <v-card class="pa-6 text-center">
+        <a-text-field label="تیتر" v-model="message.title"/>
+        <v-textarea outlined label="توضیح" v-model="message.desc" class="mt-4"/>
+        <v-btn @click="sendDirectMessage" :loading="l.sendMessage">ارسال</v-btn>
+      </v-card>
+    </v-dialog>
+  </div>
+
 </template>
 
 <script>
@@ -84,7 +99,9 @@ export default {
   },
   data() {
     return {
-      l: {apply: false, downloadVideo: false},
+      l: {apply: false, downloadVideo: false, sendMessage: false},
+      d: {sendMessage: false},
+      message: {user_id: this.$route.params.id, title: '', desc: ''},
       state: {
         email: false,
         cell_phone: false,
@@ -137,7 +154,17 @@ export default {
 
       fileLink.click();
       this.l.downloadVideo = false
-    }
+    },
+
+    onMessage() {
+      this.d.sendMessage = true
+    },
+    async sendDirectMessage() {
+      this.l.sendMessage = true
+      await this.$axios.$post('/messages/send_direct', this.message);
+      this.l.sendMessage = false
+      this.d.sendMessage = false
+    },
   }
 }
 </script>
