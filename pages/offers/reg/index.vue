58a5268880a4d267e154547f8f5f910af11cc8b7
abbas <arkantos1482@gmail.com>
2021-11-10 14:19:13 +0330

    temp drc down

--- pages/offers/reg/index.vue
+++ pages/offers/reg/index.vue
@@ -236,6 +236,7 @@ export default {
   },
   computed: {
     isDRC() {
+      return false
       return [this.baseAsset, this.counterAsset].includes('DRC')
     },
     pairAssetList() {
@@ -350,15 +351,15 @@ export default {
     return {
       exchangeAction: 'buy',
       list: [
-        ['DRC/IRR', 'BTC/IRR', 'ETH/IRR', 'BNB/IRR', 'BCH/IRR', 'LTC/IRR', 'USDT/IRR', 'TRX/IRR', 'DOGE/IRR'],
-        ['DRC/USDT', 'BTC/USDT', 'ETH/USDT', 'BNB/USDT', 'BCH/USDT', 'LTC/USDT', 'TRX/USDT', 'DOGE/USDT'],
-        ['DRC/BNB', 'ETH/BTC', 'TRX/BTC', 'DOGE/BTC', 'BCH/BTC', 'LTC/BTC', 'BNB/BTC',
+        [ 'BTC/IRR', 'ETH/IRR', 'BNB/IRR', 'BCH/IRR', 'LTC/IRR', 'USDT/IRR', 'TRX/IRR', 'DOGE/IRR'],
+        [ 'BTC/USDT', 'ETH/USDT', 'BNB/USDT', 'BCH/USDT', 'LTC/USDT', 'TRX/USDT', 'DOGE/USDT'],
+        [ 'ETH/BTC', 'TRX/BTC', 'DOGE/BTC', 'BCH/BTC', 'LTC/BTC', 'BNB/BTC',
           'TRX/ETH', 'DOGE/ETH', 'BCH/ETH', 'LTC/ETH', 'BNB/ETH',
           'BNB/BCH', 'TRX/BNB', 'DOGE/BNB']],
       offers: [],
       tabIndex: 0,
-      pairAsset: 'DRC/IRR',
-      baseAsset: 'DRC',
+      pairAsset: 'BTC/IRR',
+      baseAsset: 'BTC',
       counterAsset: 'IRR',
       interval: {offers: null, trades: null},
       windowWidth: window.innerWidth,
