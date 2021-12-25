a0ff1da52a992ed4950061e78aa085bc8590d412
abbas <arkantos1482@gmail.com>
2021-12-25 20:07:25 +0330

    accounting report

--- pages/admin/AggregatedReports.vue
+++ pages/admin/AggregatedReports.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="text-center mt-16">
+  <div class="text-center my-8">
     <v-row justify="center">
       <v-btn :href="trade_link">دریافت تمام معاملات</v-btn>
     </v-row>
+
     <v-divider class="my-8"/>
+
     <v-row justify="center" align="baseline">
       <v-col cols="3">
         <v-select filled hint="فیلتر" v-model="payment_filter" :items="payment_filter_list"/>
@@ -12,6 +14,13 @@
         <v-btn :href="payment_link">دریافت تمام پرداخت ها</v-btn>
       </v-col>
     </v-row>
+
+    <v-divider class="mb-8"/>
+
+    <v-row justify="center">
+      <v-btn :href="trade_accounting_link">گزارش معاملات حسابداری</v-btn>
+    </v-row>
+
   </div>
 </template>
 
@@ -22,6 +31,9 @@ export default {
     payment_filter_list: () => ['trade_fee', 'referral', 'deposit', 'withdraw'],
     trade_link() {
       return this.$axios.defaults.baseURL + '/trades/all/export'
+    },
+    trade_accounting_link() {
+      return this.$axios.defaults.baseURL + '/trades/accounting/all/export'
     },
     payment_link() {
       return this.$axios.defaults.baseURL + '/payments/all/export?filter=' + this.payment_filter
