d2e8208147bcb3258b1f927f1223522fa432b17a
abbas <abbas_mhosseini@yahoo.com>
2022-01-10 04:10:47 +0330

    trade fee bills commit

--- pages/admin/AggregatedReports.vue
+++ pages/admin/AggregatedReports.vue
@@ -21,6 +21,18 @@
       <v-btn :href="trade_accounting_link">گزارش معاملات حسابداری</v-btn>
     </v-row>
 
+    <v-divider class="my-8"/>
+
+    <v-row justify="center" align="baseline">
+      <v-col cols="4">
+        <v-btn :href="all_trade_bills_link">دریافت همه صورت حساب ها</v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field filled label="شناسه کاربر" v-model="user_id"/>
+        <v-btn :href="user_trade_bills_link">دریافت صورت حساب فردی</v-btn>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
@@ -35,13 +47,20 @@ export default {
     trade_accounting_link() {
       return this.$axios.defaults.baseURL + '/trades/accounting/all/export'
     },
+    all_trade_bills_link() {
+      return this.$axios.defaults.baseURL + '/trade-fee-bills'
+    },
+    user_trade_bills_link() {
+      return this.$axios.defaults.baseURL + '/trade-fee-bills/' + this.user_id
+    },
     payment_link() {
       return this.$axios.defaults.baseURL + '/payments/all/export?filter=' + this.payment_filter
     }
   },
   data() {
     return {
-      payment_filter: 'withdraw'
+      payment_filter: 'withdraw',
+      user_id: ''
     }
   }
 
