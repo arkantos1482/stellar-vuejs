65cc89468a1aa066dd74dd8f00f48fd0d8958282
abbas <arkantos1482@gmail.com>
2022-06-19 13:17:02 +0430

    change regex from lookbehind

--- models/NumberUtil.js
+++ models/NumberUtil.js
@@ -1,8 +1,13 @@
 import Decimal from "decimal.js-light";
 
-export const toSeparated = (val) => val.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+// export const toSeparated = (val) => val.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+export const toSeparated = (val) => {
+    let parts = val.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
 
-export const remSeparated = (val) => val.toString().replaceAll(',', '')
+// export const remSeparated = (val) => val.toString().replaceAll(',', '')
 
 export const safeDecimal = (val) => {
     try {
@@ -12,14 +17,14 @@ export const safeDecimal = (val) => {
     }
 }
 
-export const decimalRegex = (dp) => {
-    let pattern
-    if (dp > 0) {
-        pattern = '^$|^(\\d+\\.?(\\d{1,' + dp + '})?)$'
-    } else {
-        pattern = '^$|^(\\d+)$'
-    }
-
-    let regex = new RegExp(pattern)
-    return regex
-}
+// export const decimalRegex = (dp) => {
+//     let pattern
+//     if (dp > 0) {
+//         pattern = '^$|^(\\d+\\.?(\\d{1,' + dp + '})?)$'
+//     } else {
+//         pattern = '^$|^(\\d+)$'
+//     }
+//
+//     let regex = new RegExp(pattern)
+//     return regex
+// }
