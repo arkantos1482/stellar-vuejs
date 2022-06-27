3ad1c5ffc93bfdf0b6cd4810cbccc218e7f66c3c
abbas <arkantos1482@gmail.com>
2022-06-27 15:35:23 +0430

    add sort and filters

--- pages/admin/user-trade-fees.vue
+++ pages/admin/user-trade-fees.vue
@@ -4,23 +4,18 @@
         ref="table"
         url="/user-trade-fee"
         :headers="headers"
-        :hide-filter="true"
-        :hidePaginate="true"
-        :filter-query="[]"
-        default-sort-by="created_at"
-        :default-sort-desc="true">
+        :filter-query="filterQuery"
+        default-sort-by="fee_ratio"
+        :default-sort-desc="false">
+      <template v-slot:item.birth_date="{value}">{{ value|toFarsiJustDate }}</template>
       <template v-slot:item.actions="{item}">
-        <v-btn outlined small color="primary" @click="editDialog(item)">ویرایش
-        </v-btn>
+        <v-btn outlined small color="success" @click="createDialog(item)">ایجاد</v-btn>
+        <v-btn outlined small color="primary" @click="editDialog(item)">ویرایش</v-btn>
         <v-btn outlined small color="error"
                @click="remove(item.id)">حذف
         </v-btn>
       </template>
     </a-paged-table>
-
-    <div class="text-center mt-4">
-      <v-btn color="primary" @click="createDialog" outlined>اضافه کردن کارمزد کاربر</v-btn>
-    </div>
 
     <v-dialog width="400" v-model="d.userTradeFee">
       <v-card class="pa-6 text-center">
@@ -53,12 +48,23 @@ export default {
       },
       d: {userTradeFee: false},
       l: {send: false},
+      filterQuery: [
+        {type: 'text', name: 'ایمیل', key: 'users.email', value: ''},
+        {type: 'text', name: 'نام', key: 'profiles.name', value: ''},
+        {type: 'text', name: 'نام خانوادگی', key: 'profiles.last_name', value: ''},
+        {type: 'text', name: 'استان', key: 'profiles.province', value: ''},
+        {type: 'time', name: 'تاریخ تولد', key: 'same_birth_date', value: ''},
+      ],
     }
   },
   computed: {
     headers: () => [
-      {value: 'user_id', text: 'شناسه کاربر', align: 'center'},
+      // {value: 'user_id', text: 'شناسه کاربر', align: 'center'},
       {value: 'email', text: 'ایمیل', align: 'center'},
+      {value: 'name', text: 'نام', align: 'center'},
+      {value: 'last_name', text: 'نام خانوادگی', align: 'center'},
+      {value: 'birth_date', text: 'تاریخ تولد', align: 'center'},
+      {value: 'province', text: 'استان', align: 'center'},
       {value: 'fee_ratio', text: 'نرخ کارمزد', align: 'center'},
       {value: 'description', text: 'توضیحات', align: 'center'},
       {value: 'actions', text: 'actions', align: 'center', width: '180'},
@@ -82,8 +88,9 @@ export default {
       this.userTradeFee = item
       this.d.userTradeFee = true
     },
-    createDialog() {
+    createDialog(item) {
       this.userTradeFee = emptyArray(this.userTradeFee)
+      this.userTradeFee.user_id = item.user_id
       this.d.userTradeFee = true
     },
     async remove(id) {
